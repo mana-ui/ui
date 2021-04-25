83017c7545b17fa76d7351d54a6e0a1a7968c3dc
qdl <levinqdl@magicengine.com.cn>
2021-04-25 16:40:16 +0800

    use design system default theme

--- src/SystemProvider.js
+++ src/SystemProvider.js
@@ -1,12 +1,13 @@
 import React from "react";
 import SystemContext from "./SystemContext";
 import { ThemeProvider, jss, JssProvider } from "react-jss";
+import defaults from 'lodash.defaultsdeep'
 
 jss.setup({insertionPoint: 'mana-insertion-point'})
 
 const SystemProvider = ({ theme, system, children }) => (
     <JssProvider jss={jss}>
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={defaults(theme, system.defaultTheme)}>
             <SystemContext.Provider value={system}>{children}</SystemContext.Provider>
         </ThemeProvider>
     </JssProvider>
