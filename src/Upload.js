ad14bcabfdc7211ef8e900b78d7b13477ddbc8a8
qdl <levinqdl@magicengine.com.cn>
2021-04-27 14:22:40 +0800

    update Upload

--- src/Upload.js
+++ src/Upload.js
@@ -1,29 +1,31 @@
-import React, {useContext, useRef} from "react";
+import React, { useContext, useRef } from "react";
 import { useTheme } from "react-jss";
 import SystemContext from "./SystemContext";
-import cx from 'classnames'
+import cx from "classnames";
 
-const Upload = ({ onChange, label, className }) => {
-  const ref = useRef()
-  const files = ref.current?.files ?? []
+const Upload = ({ onChange, label, className, children, placeholder }) => {
+  const ref = useRef();
+  const files = ref.current?.files ?? [];
   const active = files.length > 0 ?? false;
   const system = useContext(SystemContext);
-  const theme = useTheme()
+  const theme = useTheme();
   const classes = system.useUploadStyles({ active, theme });
   return (
-    <div className={cx(classes.wrapper, className)} onClick={() => ref.current.click()}>
+    <div className={cx(classes.wrapper, className)}>
+      {label && <label className={classes.label}>{label}</label>}
+      {active ? <div className={classes.list}>
+        {[].map.call(files, (file) => {
+          return <div key={file.name}>{file.name}</div>;
+        })}
+      </div> :<div className={classes.placeholder}>{ placeholder }</div>}
+      {children}
       <input
         ref={ref}
         type="file"
-        onChange={onChange}
+        onChange={( {target: {files}} ) => {
+          onChange(files)}}
         className={classes.input}
       />
-      <label className={classes.label}>{label}</label>
-      <div className={classes.list}>
-        {[].map.call(files, file => {
-           return <div>{file.name}</div>
-        })}
-      </div>
     </div>
   );
 };
