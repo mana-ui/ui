d30bc16dd3b3e04664fff9d1443978ca842d29b3
qdl <levinqdl@magicengine.com.cn>
2021-06-29 13:55:07 +0800

    rerender Dialog with parent component

--- src/DialogContext.js
+++ src/DialogContext.js
@@ -1,24 +1,44 @@
-import { Children, createContext, useContext, useMemo, useState } from "react";
+import { Children, createContext, useCallback, useContext, useEffect, useMemo, useRef, useState } from "react";
 import {AnimatePresence} from 'framer-motion'
 
 const Context = createContext();
 
 export const DialogProvider = ({ children }) => {
-  const [modals, setModals] = useState([]);
-  const ctxValue = useMemo(() => {
-    return [
-      (modal) => setModals((modals) => [...modals, modal]),
-      () => setModals((modals) => modals.slice(0, -1)),
-    ];
-  });
+	const idRef = useRef(0)
+  const [dialogs, setDialogs] = useState({});
+	const ctxValue = useMemo(() => ([
+		(dialog, id) => {
+			id = id ?? idRef.current++
+			setDialogs(dialogs => ({...dialogs, [id]: dialog}))
+			return id
+		},
+		(id) => {
+			setDialogs(dialogs => {
+				delete dialogs[id]
+				return {...dialogs}
+			})
+		}
+	]), [])
   return (
     <Context.Provider value={ctxValue}>
       {children}
-      <AnimatePresence>{Children.map(modals, modal => modal)}</AnimatePresence>
+      <AnimatePresence>{Children.map(Object.values(dialogs), dialog => dialog)}</AnimatePresence>
     </Context.Provider>
   );
 };
 
-export const useDialog = () => {
-  return useContext(Context);
+export const useDialog = (dialog) => {
+	const [isVisible, setVisible] = useState()
+	const idRef = useRef()
+	const [open, close ] = useContext(Context)
+	const closeDialog = useCallback(() => {
+		setVisible(false)
+		close(idRef.current)
+	}, [])
+	useEffect(() => {
+		if (isVisible) {
+			idRef.current = open(dialog(closeDialog), idRef.current)
+		}
+	})
+	return useMemo(() => [() => setVisible(true), closeDialog], [])
 };
