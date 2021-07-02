52404045addac9a9ddb6c9b8ec765a582e9bc25e
qdl <levinqdl@magicengine.com.cn>
2021-07-02 18:27:33 +0800

    DataGrid: sticky header

--- src/DataGrid.js
+++ src/DataGrid.js
@@ -1,17 +1,26 @@
 import React, { useContext } from "react";
 import SystemContext from "./SystemContext";
 import cx from "classnames";
+import styled from '@emotion/styled'
 
+const Container = styled.div`position: relative;`
+const Th = styled.th`
+	position: sticky;
+	top: 0;
+	background: #fff;
+	box-shadow: inset 0 -1px 0 rgba(0,0,0,.12);
+	z-index: 1;
+`
 
 export const DataGrid = ({ children,  data=[], rowKey, className, empty = "No Data" }) => {
 	const system = useContext(SystemContext);
 	const {Empty} = system
   const classes = system.useDataGridStyles();
   return (
-      <div className={cx(classes.container, className)}>
+      <Container className={cx(classes.container, className)}>
       <table className={classes.table}>
         <thead>
-          <tr className={classes.headerRow}>
+          <tr className={classes.headerTr}>
 						{children}
           </tr>
         </thead>
@@ -29,12 +38,12 @@ export const DataGrid = ({ children,  data=[], rowKey, className, empty = "No Da
           )): <tr className={classes.tr}><td colSpan={React.Children.count(children)}><Empty>{empty}</Empty></td></tr>}
         </tbody>
       </table>
-    </div>
+    </Container>
   );
 };
 
 export const Column = ({header}) => {
 	const system = useContext(SystemContext);
   const classes = system.useDataGridStyles();
-  return <th className={cx(classes.cell, classes.th)}>{header}</th>;
+  return <Th className={cx(classes.cell, classes.th)}>{header}</Th>;
 };
